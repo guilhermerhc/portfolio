8e5eec353d3a8dbd340e7db8dc1f14863ec10d13
guilhermerhc <guilhermerhc@gmail.com>
2023-05-19 14:00:30 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 function navigate(linkedin) {
-    window.open("https://www.linkedin.com/in/guilhermerhc", '_parent').focus();
+    window.open("https://www.linkedin.com/in/guilhermerhc", '_blank').focus();
 }
